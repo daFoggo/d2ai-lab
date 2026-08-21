f89d86bfbde0917703916e4798eda6c4c5adfa63
daFoggo <ntgiang141105@gmail.com>
2026-08-21 16:56:30 +0700

    fix: fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute("/")({
 function Home() {
 	return (
 		<div className="flex min-h-screen items-center justify-center p-4">
-			<h1 className="text-xl font-semibold">D2AI Lab</h1>
+			<h1 className="text-xl font-semibold">D2AI Lab bonjour font testing</h1>
 		</div>
 	);
 }
